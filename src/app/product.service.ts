69c4ed6742f4df4c7543225f692e741f7f9455f7
Abhishek Kaushik <abhishek.kr.kaushik@gmail.com>
2020-12-16 19:28:52 +0530

    Products List

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product';
 
 
 @Injectable()
@@ -12,9 +13,17 @@ export class ProductService {
 
   private _albumUrl: string = '../assets/album.json';
 
+  private _productsUrl: string = '../assets/products.json';
+
   getAlbum(id:number):Observable<Album>{
     return this._http.get(this._albumUrl).map(
       response => <Album> response.json()
+    );
+  }
+
+  getProducts():Observable<Product[]>{
+    return this._http.get(this._productsUrl).map(
+      response => <Product[]>response.json()
     );
   }
 
